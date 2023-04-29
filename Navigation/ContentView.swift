55b3047f79e5ce0ab356ee92d2659c359e64d06e
Emma Finkel <emma.finkel@gmail.com>
2023-04-29 18:02:11 -0400

    commit 1

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,18 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            VStack {
+                Text("This is the root view 🌳")
+            }
+            .toolbar {
+                
+                
+                NavigationLink(destination: SecondSwiftUIView()) {
+                    Text("About")
+                    
+                    
+                    
+                }
+                
+            }
+            
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
---
+++ Navigation/SecondSwiftUIView.swift
@@ -0,0 +1,24 @@
+//
+//  SecondSwiftUIView.swift
+//  Navigation
+//
+//  Created by scholar on 4/23/23.
+//
+
+import SwiftUI
+
+struct SecondSwiftUIView: View {
+    var body: some View {
+        
+        VStack {
+            Text("You've made it to the second view!")
+        }
+
+    }
+}
+
+struct SecondSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondSwiftUIView()
+    }
+}
